87d9ecd86a3a05b2dd3bf05fff8e107853e2a103
gamikasa <73681995+gamikasa@users.noreply.github.com>
2022-08-30 11:43:33 +0700

    week4

---
+++ src/main/java/com/example/week4/MathView.java
@@ -0,0 +1,11 @@
+//package com.example.week4;
+//
+//import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+//import com.vaadin.flow.router.Route;
+//
+//@Route
+//public class MathView extends VerticalLayout {
+//    public View() {
+//
+//    }
+//}
